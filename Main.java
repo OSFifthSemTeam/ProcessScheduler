3bad6c9673409bc2fe01b5e9be41fa1f9dbea41d
YasaswiK <yasaswik.bcl2@gmail.com>
2015-09-13 22:36:23 +0530

    Added prelim Main stuff

--- Main.java
+++ Main.java
@@ -2,4 +2,18 @@ public class Main
 {
 	public static void main(String args[])
 	{
-		
+		/* Call 
+		ConfigInput
+		ReadJobs
+		ReadModelFiles */
+
+		int current_time=0;
+		while (true)
+		{
+			InputTable.checkfornewprocess(current_time)
+			Ready.update(SystemConfig.psg)
+			IOQueues.update(SystemConfig.psg)
+			MemQueue.update()	
+		}
+	}
+}
